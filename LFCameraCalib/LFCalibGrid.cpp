ba0b0a9b715a4ddc97b874d5bc3042112a5af5f2
anakinhe <uwenda@outlook.com>
2020-09-08 21:30:08 +0800

    Update calibration program

---
+++ LFCameraCalib/LFCalibGrid.cpp
@@ -0,0 +1,190 @@
+#include "common.hpp"
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <signal.h>
+
+#include <opencv2/opencv.hpp>
+
+#include "calibration.hpp"
+
+namespace chrono = std::chrono;
+class Processor : public MVCCamera
+{
+public:
+    Processor() : left(70), right(70), top(60), bottom(90), medianBlurSize(7), blockSize(31), C(1), openOpBlockSize(13)
+    {
+        cv::namedWindow("Parameter");
+        cv::namedWindow("Process");
+        cv::createTrackbar("medianBlurSize", "Parameter", &medianBlurSize, 60);
+        cv::createTrackbar("blockSize", "Parameter", &blockSize, 60);
+        cv::createTrackbar("C", "Parameter", &C, 60);
+        cv::createTrackbar("enrodBlockSize", "Parameter", &openOpBlockSize, 60);
+
+        cv::createTrackbar("left", "Parameter", &left, 100);
+        cv::createTrackbar("right", "Parameter", &right, 100);
+        cv::createTrackbar("top", "Parameter", &top, 100);
+        cv::createTrackbar("bottom", "Parameter", &bottom, 100);
+    }
+    virtual ~Processor() {}
+    virtual UINT Process(MVCFRAMEINFO &m_FrameInfo)
+    {
+        std::stringstream ss;
+        ss << m_FrameInfo.FRAMEID;
+        static cv::Mat img_gray, img_bgr;
+        chrono::time_point<chrono::steady_clock> tp_begin = chrono::steady_clock::now();
+        m_Raw = cv::Mat(cv::Size(m_FrameInfo.Width, m_FrameInfo.Height), CV_8UC1, m_FrameInfo.lBufPtr);
+
+        m_Mutex.lock();
+        cv::cvtColor(m_Raw, m_BGR, cv::COLOR_BayerGB2BGR);
+        m_Mutex.unlock();
+
+        int ratio = m_BGR.cols / 800;
+        cv::Size sz(m_BGR.cols / ratio, m_BGR.rows / ratio);
+        cv::resize(m_BGR, img_bgr, sz);
+        cv::imshow("Raw", img_bgr);
+        cv::waitKey(1);
+
+        return 0u;
+    }
+
+    void ShowBinary()
+    {
+        if (m_BGR.empty())
+        {
+            std::cerr << __FILE__ << "(" << __LINE__ << "): m_BGR.empty()==true\n";
+            return;
+        }
+        static cv::Mat viewImage;
+        m_Mutex.lock();
+        cv::cvtColor(m_BGR, m_Gray, cv::COLOR_BGR2GRAY);
+        m_Mutex.unlock();
+
+        GetBinaryImage(m_Gray, m_BinaryGray);
+        int ratio = m_BinaryGray.cols / 800;
+        cv::Size sz(m_BinaryGray.cols / ratio, m_BinaryGray.rows / ratio);
+        cv::resize(m_BinaryGray, viewImage, sz);
+        // cv::cvtColor(viewImage, viewImage, cv::COLOR_GRAY2BGR);
+        cv::imshow("Process", viewImage);
+    }
+
+    void DrawCenters()
+    {
+        if (m_Gray.empty())
+        {
+            std::cerr << __FILE__ << "(" << __LINE__ << "): m_Gray.empty()==true\n";
+            return;
+        }
+        static cv::Mat viewImage;
+        m_Centers.clear();
+        anakin::DetectCenters(m_BinaryGray, m_Centers);
+        m_Centers = anakin::CropBorder(m_Centers, m_BinaryGray.cols, m_BinaryGray.rows, left, right, top, bottom);
+        m_Mutex.lock();
+        m_BGR.copyTo(viewImage);
+        m_Mutex.unlock();
+
+        for (auto &p : m_Centers)
+        {
+            cv::circle(viewImage, p, 10, cv::Scalar(0, 0, 255), 7);
+        }
+        int ratio = viewImage.cols / 800;
+        cv::Size sz(viewImage.cols / ratio, viewImage.rows / ratio);
+        cv::resize(viewImage, viewImage, sz);
+        cv::imshow("Process", viewImage);
+    }
+
+    void CalibGridMatrix()
+    {
+        static cv::Mat viewImage;
+        m_Mutex.lock();
+        m_BGR.copyTo(viewImage);
+        m_Mutex.unlock();
+
+        cv::Mat GridMatrix = anakin::DetectGridMatrix(m_Centers);
+        std::cout << "Grid Matrix: " << GridMatrix << "\n";
+        for (int row = 0;; ++row)
+        {
+            int col;
+            for (col = 0;; ++col)
+            {
+                cv::Mat m = (cv::Mat_<double>(1, 3) << col, row, 1) * GridMatrix;
+                cv::Point point((int)m.at<double>(0), (int)m.at<double>(1));
+                if (point.inside(cv::Rect2i(0, 0, m_Gray.cols, m_Gray.rows)))
+                {
+                    cv::circle(viewImage, point, 10, cv::Scalar(255, 255, 0), 5);
+                }
+                else
+                    break;
+            }
+            if (col == 0)
+                break;
+        }
+        int ratio = viewImage.cols / 800;
+        cv::Size sz(viewImage.cols / ratio, viewImage.rows / ratio);
+        cv::resize(viewImage, viewImage, sz);
+        cv::imshow("Process", viewImage);
+    }
+
+private:
+    void GetBinaryImage(cv::Mat &src_gray_img, cv::Mat &dst_binary_img)
+    {
+        if (src_gray_img.channels() != 1)
+        {
+            std::cerr << __FILE__ << "(" << __LINE__ << "): src_gray_img.channels() != 1\n";
+            return;
+        }
+        cv::medianBlur(src_gray_img, dst_binary_img, medianBlurSize * 2 + 1);
+        cv::adaptiveThreshold(dst_binary_img, dst_binary_img, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, blockSize * 2 + 3, static_cast<double>(C) / 100.0);
+        cv::morphologyEx(dst_binary_img, dst_binary_img, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(openOpBlockSize * 2 + 1, openOpBlockSize * 2 + 1)));
+    }
+
+    cv::Mat m_Raw, m_BGR, m_Gray, m_BinaryGray;
+    std::vector<cv::Point2d> m_Centers;
+
+    int medianBlurSize, blockSize, C, openOpBlockSize;
+    int left, right, top, bottom;
+    std::mutex m_Mutex;
+};
+
+bool isOk = true;
+
+void onSig(int sig)
+{
+    isOk = false;
+}
+
+int main()
+{
+    signal(SIGINT, onSig);
+    std::string cmd;
+    std::shared_ptr<MVCCamera> pCamera(new Processor);
+    pCamera->PrintDevices();
+    std::cout << pCamera->Open(0) << "\n";
+    std::cout << pCamera->StartCapture() << "\n";
+    char key;
+    while (isOk)
+    {
+        // std::this_thread::sleep_for(std::chrono::seconds(5));
+        key = cv::waitKey(1);
+
+        if (key == 27)
+            break;
+        else if (key == '1')
+        {
+            dynamic_cast<Processor *>(&*pCamera)->ShowBinary();
+        }
+        else if (key == '2')
+        {
+            dynamic_cast<Processor *>(&*pCamera)->DrawCenters();
+        }
+        else if (key == '3')
+        {
+            dynamic_cast<Processor *>(&*pCamera)->CalibGridMatrix();
+        }
+    }
+
+    pCamera->StopCapture();
+    pCamera->Close();
+    return 0;
+}
---
+++ LFCameraCalib/calibration.hpp
@@ -0,0 +1,169 @@
+#ifndef CALIBRATION_INCLUDE_HPP
+#define CALIBRATION_INCLUDE_HPP
+
+#include <vector>
+#include <opencv2/core/types.hpp>
+
+namespace anakin
+{
+    template <typename PType>
+    void DetectCenters(cv::Mat &binMat, std::vector<PType> &centers);
+
+    template <typename PType>
+    std::vector<PType> CropBorder(std::vector<PType> centers, size_t w, size_t h, size_t left, size_t right, size_t top, size_t bottom);
+
+    template <typename PType>
+    cv::Mat DetectGridMatrix(std::vector<PType> centers, double estimate_dist = 0);
+
+    template <typename PType>
+    double EstimateAverageDist(std::vector<PType> centers);
+
+    template <typename PType>
+    int IsNeighbor(const PType &a, const PType &b, double estiname_dist, double dist_thresh);
+
+    template <typename PType>
+    void DetectCenters(cv::Mat &binMat, std::vector<PType> &centers)
+    {
+        std::vector<std::vector<cv::Point>> contours;
+        std::vector<cv::Vec4i> hierachy;
+
+        cv::findContours(binMat, contours, hierachy, cv::CHAIN_APPROX_SIMPLE, cv::RETR_TREE);
+        size_t sz = contours.size();
+        for (size_t i = 0; i < sz; ++i)
+        {
+            if (hierachy[i][3] >= 0)
+                continue;
+            cv::RotatedRect r = cv::minAreaRect(contours[i]);
+            centers.push_back(r.center);
+        }
+    }
+
+    template <typename PType>
+    std::vector<PType> CropBorder(std::vector<PType> centers, size_t w, size_t h, size_t left, size_t right, size_t top, size_t bottom)
+    {
+        for (auto i = centers.begin(); i != centers.end();)
+            i = (i->x < left || i->x >= w - right || i->y < top || i->y >= h - bottom) ? centers.erase(i) : i + 1;
+        return centers;
+    }
+
+    template <typename PType>
+    cv::Mat DetectGridMatrix(std::vector<PType> centers, double estimate_dist)
+    {
+        if (centers.empty())
+        {
+            std::cerr << __FILE__ << "(" << __LINE__ << "): centers.empty()==true\n";
+            return cv::Mat();
+        }
+        size_t items_num = centers.size();
+        struct Aperture
+        {
+            Aperture(PType p_, int row_, int col_):p(p_), row(row_), col(col_){}
+            PType p;
+            int row, col;
+        };
+        if (abs(estimate_dist) <= DBL_EPSILON)
+            estimate_dist = EstimateAverageDist(centers);
+        std::sort(centers.begin(), centers.end(), [](const PType &a, const PType &b) { return (a.x + a.y) > (b.x + b.y); });
+        cv::Mat A, B;
+        std::queue<Aperture> q;
+        q.push(Aperture(centers.back(), 0, 0));
+        centers.pop_back();
+        while (!q.empty())
+        {
+            Aperture aper = q.front();
+            q.pop();
+
+            A.push_back(static_cast<double>(aper.col));
+            A.push_back(static_cast<double>(aper.row));
+            A.push_back(1.0);
+            B.push_back(aper.p.x);
+            B.push_back(aper.p.y);
+
+            for (auto it = centers.begin(); it != centers.end();)
+            {
+                int dir = IsNeighbor(aper.p, *it, estimate_dist, 25);
+                switch (dir)
+                {
+                case 0:
+                {
+                    q.push(Aperture(*it, aper.row - 1, aper.col));
+                    it = centers.erase(it);
+                    break;
+                }
+                case 1:
+                {
+                    q.push(Aperture(*it, aper.row + 1, aper.col));
+                    it = centers.erase(it);
+                    break;
+                }
+                case 2:
+                {
+                    q.push(Aperture(*it, aper.row, aper.col - 1));
+                    it = centers.erase(it);
+                    break;
+                }
+                case 3:
+                {
+                    q.push(Aperture(*it, aper.row, aper.col + 1));
+                    it = centers.erase(it);
+                    break;
+                }
+                default:
+                    it++;
+                }
+            }
+        }
+        
+        try
+        {
+            A = A.reshape(1, items_num);
+	        B = B.reshape(1, items_num);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+            std::cerr << items_num << "\n" << A.size << "\n" << B.size << "\n";
+            return cv::Mat();
+        }
+        
+        cv::Mat Matrix;
+        cv::solve(A, B, Matrix, cv::DECOMP_SVD);
+        return Matrix;
+    }
+
+    template <typename PType>
+    double EstimateAverageDist(std::vector<PType> centers)
+    {
+        // std::sort(centers.begin(), centers.end(), [](const PType &a, const PType &b) { return (a.x + a.y) < (b.x + b.y); });
+        std::vector<double> dist_vec;
+        while (centers.size() > 1)
+        {
+            PType p = centers.back();
+            centers.pop_back();
+            auto it_closest = centers.begin();
+            for (auto it = centers.begin(); it != centers.end(); ++it)
+            {
+                if (cv::norm(*it - p) < cv::norm(*it_closest - p))
+                    it_closest = it;
+            }
+            dist_vec.push_back(cv::norm(*it_closest - p));
+            centers.erase(it_closest);
+        }
+        std::sort(dist_vec.begin(), dist_vec.end());
+        return dist_vec[dist_vec.size() / 2];
+    }
+
+    template <typename PType>
+    int IsNeighbor(const PType &a, const PType &b, double estiname_dist, double dist_thresh)
+    {
+        PType dir[4] = {PType(0, -estiname_dist), PType(0, estiname_dist), PType(-estiname_dist, 0), PType(estiname_dist, 0)};
+        for (int i = 0; i < 4; ++i)
+        {
+            if (cv::norm(a + dir[i] - b) < dist_thresh)
+                return i;
+        }
+        return -1;
+    }
+} // namespace anakin
+
+#endif
