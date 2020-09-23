0a7d7fac57385529c95e77e32f0c9f60c8af415b
anakinhe <uwenda@outlook.com>
2020-09-23 20:14:57 +0800

    修正曝光时间范围

--- camera_gui.cpp
+++ camera_gui.cpp
@@ -6,6 +6,6 @@ MVCCameraGUI::MVCCameraGUI() : m_Exposure(3431), m_Gain(100)
     cv::namedWindow("Parameter");
     cv::resizeWindow("Parameter", cv::Size(400, 600));
     
-    cv::createTrackbar("Exposure", "Parameter", &m_Exposure, 3431);
+    cv::createTrackbar("Exposure", "Parameter", &m_Exposure, 34464);
     cv::createTrackbar("Gain", "Parameter", &m_Gain, 126);
 }
